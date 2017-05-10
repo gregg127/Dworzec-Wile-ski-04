b304c52abb6cf106d6ea280f388be179ea703ba0
gregg127 <grzechu881@gmail.com>
2017-05-10 16:32:40 +0200

    Watek sciagajacy zrodlo strony, nowy przystanek, ikona

---
+++ app/src/main/java/com/example/grzesiek/dworzecwilenski04/Handlowa.java
@@ -0,0 +1,98 @@
+package com.example.grzesiek.dworzecwilenski04;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.EditText;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Grzesiek on 2017-05-10.
+ */
+
+public class Handlowa extends AppCompatActivity {
+    final String site = "http://www.ztm.waw.pl/rozklad_nowy.php?c=182&l=1&n=1139&o=03";
+
+    private boolean czyWystapilBlad;
+    private boolean ready;
+    private String source;
+    private CheckedTextView output;
+    private Button button;
+    private EditText mEdit;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.handlowa_lay);
+
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                getSourceCode(); //sets the HTML code to source variable
+                ready = true;
+            }
+        }).start();
+
+        mEdit = (EditText)findViewById(R.id.timeTextFieldHandlowa);
+        output = (CheckedTextView)findViewById(R.id.outputHandlowa);
+        output.setMovementMethod(new ScrollingMovementMethod());
+        button = (Button) findViewById(R.id.checkScheduleHandlowa);
+        button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                if(!ready){
+                    output.setText("Pobieram rozkład, poczekaj chwilę i spróbuj ponownie.");
+                }
+                else if(czyWystapilBlad){ // in getSourceCode() exception thrown case
+                    output.setText(source);
+                    czyWystapilBlad = false;
+                    getSourceCode();
+                } else {
+                    String godzina = mEdit.getText().toString();
+                    output.setText(getRozkladJazdy(godzina));
+                }
+            }
+        });
+    }
+    private String getRozkladJazdy(String godz){
+        //================= PERMISSION PROBLEMS
+        //http://stackoverflow.com/questions/17360924/securityexception-permission-denied-missing-internet-permission
+        //================
+
+        String regex = "\\s("+godz+":\\d{2})(.*?)\\(";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(source);
+        ArrayList<String> list = new ArrayList<>();
+        while(m.find()){
+            list.add(m.group(1) + "   ||" + m.group(2));
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String e : list){
+            sb.append(e);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void getSourceCode(){ // === WYKORZYSTANIE JSOUP
+        try {
+            Document docTemp = Jsoup.connect(site).get();
+            source = docTemp.text();
+        } catch(Exception ex){
+            source = "Wystąpił błąd przy ładowaniu strony.\nWłącz internet i spróbuj ponownie.";
+            czyWystapilBlad = true;
+        }
+    }
+}
--- app/src/main/java/com/example/grzesiek/dworzecwilenski04/MainActivity.java
+++ app/src/main/java/com/example/grzesiek/dworzecwilenski04/MainActivity.java
@@ -1,102 +1,32 @@
 package com.example.grzesiek.dworzecwilenski04;
 
-import android.os.Build;
+import android.content.Intent;
 import android.os.StrictMode;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckedTextView;
-import android.widget.EditText;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-    final String site = "http://www.ztm.waw.pl/rozklad_nowy.php?c=182&l=1&n=1003&o=04";
-
-    boolean czyWystapilBlad;
-    String source;
-    CheckedTextView output;
-    Button button;
-    EditText mEdit;
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Two very important lines
+        Button wilenski = (Button) findViewById(R.id.wilenskiButton);
+        Button handlowa = (Button) findViewById(R.id.handlowaButton);
+
+        //Internet access thing
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-
-        getSourceCode(); //sets the HTML code to source variable
-
-        mEdit = (EditText)findViewById(R.id.editText);
-        output = (CheckedTextView)findViewById(R.id.checkedTextView);
-        output.setMovementMethod(new ScrollingMovementMethod());
-        button = (Button) findViewById(R.id.buttonOne);
-        button.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-            @Override
-            public void onClick(View v) {
-                if(czyWystapilBlad){ // w przypadku wyrzucenia wyjatku przy getSourceCode()
-                    output.setText(source);
-                    czyWystapilBlad = false;
-                    getSourceCode();
-                }else {
-                    String godzina = mEdit.getText().toString();
-                    output.setText(getRozkladJazdy(godzina));
-                }
-            }
-        });
-    }
-    private String getRozkladJazdy(String godz){
-
-        //================= PERMISSION PROBLEMS look down below
-        //http://stackoverflow.com/questions/17360924/securityexception-permission-denied-missing-internet-permission
-        //================
-
-        //String regex = "<div class=\"wwgodz\">(" + godz + ":\\d{1,2}).*?<strong>(\\d{1,3}).*?\\d{2}\">(.*?)<span";
-        String regex = "\\s("+godz+":\\d{2})(.*?)\\(";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(source);
-        ArrayList<String> list = new ArrayList<>();
-        while(m.find()){
-            //list.add(m.group(1) + " || " + m.group(2) + " >> " + m.group(3));
-            list.add(m.group(1) + "   ||" + m.group(2));
-        }
-        StringBuilder sb = new StringBuilder();
-        for(String e : list){
-            sb.append(e);
-            sb.append("\n");
-        }
-        return sb.toString();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private void getSourceCode(){ // === WYKORZYSTANIE JSOUP
-        try{
-            // === BAD CHOICE
-            /*connection = new URL("http://www.ztm.waw.pl/rozklad_nowy.php?c=182&l=1&n=1139&o=03").openConnection();
-            Scanner sc = new Scanner(connection.getInputStream(), StandardCharsets.UTF_8.name());
-            sc.useDelimiter("\\Z");
-            String tempHtml = sc.next();
-            sc.close();*/
+    public void goToWilenski(View view){
+        startActivity( new Intent(this, Wilenski.class) );
+    }
 
-            // === GOOD CHOICE
-            Document docTemp = Jsoup.connect(site).get();
-            source = docTemp.text();
-        } catch(Exception ex){ // DODAJ OBSLUGE BLEDU W PRZYPADKU BRAKU INTERNETU
-            source = "Wystąpił błąd przy ładowaniu strony.\nWłącz internet i spróbuj ponownie.";
-            czyWystapilBlad = true;
-        }
+    public void goToHandlowa(View view){
+        startActivity( new Intent(this, Handlowa.class) );
     }
 }
